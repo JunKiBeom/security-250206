d15c22c8ab679e4e7aec4e8c9d501c0741b4d1d2
JunKiBeom <dona0408@gmail.com>
2025-02-07 09:26:40 +0900

    WithUserDetails fix

--- src/main/java/com/example/securityExam/global/security/CustomUserDetailService.java
+++ src/main/java/com/example/securityExam/global/security/CustomUserDetailService.java
@@ -20,7 +20,6 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // username, password, authorities
         Member member = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
 
