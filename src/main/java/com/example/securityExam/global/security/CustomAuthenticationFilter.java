1d362450eb03a9a759702a6f5ea57c513cd340e6
JunKiBeom <dona0408@gmail.com>
2025-02-07 10:23:19 +0900

    setLogin 추가

--- src/main/java/com/example/securityExam/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/securityExam/global/security/CustomAuthenticationFilter.java
@@ -1,19 +1,27 @@
 package com.example.securityExam.global.security;
 
+import com.example.securityExam.global.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final Rq rq;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        rq.setLogin("user1"); //    user1 로그인이 온다
+
         filterChain.doFilter(request, response); // 다음 단계로 통과
     }
 }
